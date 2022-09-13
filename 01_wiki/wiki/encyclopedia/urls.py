d6927ef34d6485335ba52b5e72c01a703f42822b
hesam-shafiei <hesamshafiei7@gmail.com>
2022-09-13 15:54:02 -0700

    .

--- 01_wiki/wiki/encyclopedia/urls.py
+++ 01_wiki/wiki/encyclopedia/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:name>", views.entryPage, name="entryPage")
+    path("wiki/<str:name>", views.entryPage, name="entryPage")
 ]
