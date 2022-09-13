64ad54cb62a24a9a9638bfdf07873b3bfce41af6
hesam-shafiei <hesamshafiei7@gmail.com>
2022-09-13 10:56:43 -0700

    got markdowner working for viewing pages

--- 01_wiki/wiki/encyclopedia/views.py
+++ 01_wiki/wiki/encyclopedia/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
 
-
+markdowner = Markdown()
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
@@ -11,9 +11,15 @@ def index(request):
 
 def entryPage(request, name):
     entryInfo = util.get_entry(name)
-
+    entryInfo_converted = None
+    isEmpty = entryInfo == None
+    if(not isEmpty):
+        entryInfo_converted = markdowner.convert(entryInfo)
+    else:
+        entryInfo_converted = None
+    
     return render(request, "encyclopedia/entryPage.html", {
         "title": name,
-        "entryInfo": entryInfo,
+        "entryInfo": entryInfo_converted,
         "isEmpty": entryInfo == None
     })
